d556be95aa23839f55c58d8752b7936f7002e7c9
MarKoz162 <Marcin.kozdras162@interia.pl>
2021-10-28 20:35:42 +0000

    Add_lessons_view

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,14 @@ class Lesson < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+  
+  def to_s
+    title
+  end  
+  
+  has_rich_text :content
+  
+  
 end
---
+++ app/policies/lesson_policy.rb
@@ -0,0 +1,32 @@
+class LessonPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+  
+  def edit?
+    @record.course.user_id == @user.id
+  end
+  
+  def update?
+    @record.course.user_id == @user.id
+  end
+  
+  def destroy?
+    @record.course.user_id == @user.id
+  end
+  
+  def show?
+    @record.course.user_id == @user.id || @user&.has_role?(:admin)
+  end 
+  
+  def new?
+    @record.course.user_id == @user.id
+  end 
+  
+  def create?
+    @record.course.user_id == @user.id
+  end  
+  
+end
