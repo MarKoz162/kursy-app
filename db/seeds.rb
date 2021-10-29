142853738d6140f3b599c0b074699ccdc9a18c16
MarKoz162 <Marcin.kozdras162@interia.pl>
2021-10-29 17:57:14 +0000

    Change_view_lessons

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,11 @@
 User.create!(email: 'Marcin.kozdras162@interia.pl', password: 'password', password_confirmation: 'password')
+user = User.new(
+  email: "user@domain.com",
+  password: 'password',
+  password_confirmation: 'password'
+)
+user.skip_confirmation!
+user.save
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
