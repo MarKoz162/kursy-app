71568497775f7cb1ef94abd3a9d65eeb777a9a1a
MarKoz162 <Marcin.kozdras162@interia.pl>
2021-10-30 13:08:38 +0000

    Add_and_modify_enrollment_view

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,14 @@
 module CoursesHelper
+  def review_button(course)
+    user_course = course.enrollments.where(user: current_user)
+    if current_user
+      if user_course.any?
+        if user_course.pending_review.any?
+          link_to 'Add areview', edit_enrollment_path(user_course.first)
+        else
+          link_to 'You have left a review.', enrollment_path(user_course.first)
+        end  
+      end      
+    end    
+  end      
 end
