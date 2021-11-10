b804e430e6ffaffd6d24cf26d2d7c49fa78ba54c
MarKoz162 <Marcin.kozdras162@interia.pl>
2021-11-10 10:54:43 +0000

    Add_comments_controller_and_model

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,33 @@
+class CommentsController < ApplicationController
+  
+  
+  def create
+    @comment = Comment.new(comment_params)
+    @course = Course.friendly.find(params[:course_id])
+    @lesson = Lesson.friendly.find(params[:lesson_id])
+    @comment.lesson_id = @lesson.id
+    @comment.user = current_user
+    if @comment.save
+       redirect_to course_lesson_path(@course,@lesson), notice: "Comment was successfully created." 
+    else
+       redirect_to course_lesson_path(@course,@lesson), alert: "Somthing went wrong." 
+    end
+  end
+  
+  def destroy
+    @course = Course.friendly.find(params[:course_id])
+    @lesson = Lesson.friendly.find(params[:lesson_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to course_lesson_path(@course, @lesson), notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end    
+  end  
+  
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:content)
+  end  
+end
