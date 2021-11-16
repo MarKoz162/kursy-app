5376ed910c04f1317411139e720039db41cf6df2
MarKoz162 <Marcin.kozdras162@interia.pl>
2021-11-16 17:45:28 +0000

    Add_rutes_and_configure_user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, :confirmable
+         :recoverable, :rememberable, :validatable, :trackable, :confirmable, 
+         :omniauthable, omniauth_providers: [:google_oauth2, :github, :facebook]
          
   rolify       
          
@@ -29,6 +30,31 @@ class User < ApplicationRecord
   after_create :assign_default_role
   
   validate :must_have_a_role, on: :update
+  
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+      user = User.create(
+        email: data['email'],
+        password: Devise.friendly_token[0,20],
+      )
+    end  
+    user.name = access_token.info.name
+    user.image = access_token.info.image
+    user.provider = access_token.provider
+    user.uid = access_token.uid
+    user.token = access_token.credentials.token
+    user.expires_at = access_token.credentials.expires_at
+    user.expires = access_token.credentials.expires
+    user.refresh_token = access_token.credentials.refresh_token
+    user.confirmed_at = Time.now
+    
+    user
+  end
+  
   
   def assign_default_role
     if User.count == 1
